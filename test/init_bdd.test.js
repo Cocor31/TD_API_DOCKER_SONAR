09775d639ff5843ad862cbb067b70ecae968172f
Cocor_Laptop <cocor@DOMI>
2023-12-13 15:59:14 +0100

    deactive sync models for workflow ci test

--- test/init_bdd.test.js
+++ test/init_bdd.test.js
@@ -4,13 +4,13 @@ const DB = require('../api/db.config')
 
 describe('MAIN ROUTER', () => {
 
-    afterAll( async () => {
-        DB.sequelize.close()
-    })    
+    afterAll(async () => {
+        await DB.sequelize.close()
+    })
 
     describe('POPULATE DATABASE', () => {
         it('Should return ?', async () => {
-            const response = await DB.sequelize.sync({alter: true})
+            const response = await DB.sequelize.sync({ alter: true })
             expect(response).toHaveProperty('models')
         })
     })
